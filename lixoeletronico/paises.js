0e1c62f1b9ad96b4f19e9abd4a979950b5a9c38f
Jhonatan Froeder <froeder3@gmail.com>
2018-10-15 00:53:39 -0400

    Inclusao navbar

---
+++ lixoeletronico/paises.js
@@ -0,0 +1,30 @@
+google.charts.load('current', {
+    'packages': ['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+        ['Country', 'Mt'],
+        ['China', 7.2],
+        ['United States', 6.3],
+        ['Japan', 2.1],
+        ['India', 2.1],
+        ['Germany', 1.9],
+        ['Brazil', 1.5],
+        ['Russia', 1.4],
+        ['France', 1.4],
+        ['Indonesia', 1.3],
+        ['Italy', 1.2]
+
+    ]);
+
+    var options = {};
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options);
+}
